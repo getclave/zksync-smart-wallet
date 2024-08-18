1e817be144ca73f4ed79fe14f7240a1f4021947d
asgarovf <fasgarov2002@gmail.com>
2024-08-18 14:37:35 +0300

    feat: clear the main page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return <main className="min-h-screen flex flex-col">{children}</main>;
+};
