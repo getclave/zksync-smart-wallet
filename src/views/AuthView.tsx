6494f9b1a0670239e91d7dea54b655ffb33db1aa
aalimsahin <alimsahin0007@gmail.com>
2024-08-29 02:24:56 -0300

    feat: added loading state

--- src/views/AuthView.tsx
+++ src/views/AuthView.tsx
@@ -1,14 +1,17 @@
 'use client';
 import { Credential, useSetCredential } from '@/store';
 import { core, Deployer, Storage, StorageKeys, Webauthn } from '@/utils';
+import { useMutation } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { FaArrowCircleRight, FaUserPlus } from 'react-icons/fa';
+import { TailSpin } from 'react-loading-icons';
 
 type BoxProps = {
     onPress: () => void;
     title: string;
     text: string;
     icon: ReactNode;
+    isPending?: boolean;
 };
 export const AuthView = () => {
     const setCredential = useSetCredential();
@@ -40,6 +43,10 @@ export const AuthView = () => {
         }
     };
 
+    const deployMutation = useMutation({
+        mutationFn: deployAccount,
+    });
+
     const loginAccount = async () => {
         const passkey = await Webauthn.login();
         const credential = {
@@ -51,6 +58,10 @@ export const AuthView = () => {
         setCredential(credential);
     };
 
+    const loginMutation = useMutation({
+        mutationFn: loginAccount,
+    });
+
     return (
         <div className="flex flex-1 justify-center items-center">
             <div className="container bg-slate-900 border-2 border-slate-800 w-[512px] min-h-[512px] max-w-[95vw] rounded-lg p-8 overflow-hidden">
@@ -59,15 +70,18 @@ export const AuthView = () => {
                 </p>
                 <div className="space-y-4">
                     <Box
-                        onPress={deployAccount}
+                        onPress={async () => {
+                            await deployMutation.mutateAsync();
+                        }}
                         icon={<FaUserPlus className="mr-2" size={32} />}
                         title="Create Smart Wallet"
-                        text=" Start deploying your Smart Wallet on ZKsync, using Clave
-              infrastructure"
+                        text=" Start deploying your Smart Wallet on ZKsync, using Clave infrastructure"
                     />
 
                     <Box
-                        onPress={loginAccount}
+                        onPress={async () => {
+                            await loginMutation.mutateAsync();
+                        }}
                         icon={<FaArrowCircleRight className="mr-2" size={32} />}
                         title="Login Existing Wallet"
                         text={`Start using your existing Smart Wallet on ZKsync`}
@@ -83,13 +97,23 @@ export const AuthView = () => {
     );
 };
 
-const Box = ({ onPress, title, text, icon }: BoxProps): ReactNode => {
+const Box = ({
+    onPress,
+    title,
+    text,
+    icon,
+    isPending = false,
+}: BoxProps): ReactNode => {
     return (
         <button
             onClick={onPress}
             className="text-white flex flex-col items-center justify-center bg-slate-950 p-4 rounded-lg w-full border-2 border-slate-800 focus:border-blue-600 min-h-[180px]"
         >
-            {icon}
+            {isPending ? (
+                <TailSpin speed={0.75} width={32} height={32} />
+            ) : (
+                icon
+            )}
             <p className="text-xl mt-2">{title}</p>
             <p className="text-sm mt-2 text-gray-400">{text}</p>
         </button>
